f17aff5075f228f98d35c25145bf596ba1fa22be
Jared Lundholm <lundholmj@gmail.com>
2016-07-13 06:49:58 -0600

    angular files

--- codecademy/angularjs/forcast/js/services/forcast.js
+++ codecademy/angularjs/forcast/js/services/forcast.js
@@ -1,9 +1,9 @@
-app.factory('forecast', ['$http', function($http) {
-  return $http.get('https://s3.amazonaws.com/codecademy-content/courses/ltp4/forecast-api/forecast.json')
-            .success(function(data) {
+app.factory('forecast', ['$http', function ($http) {
+  return $http.get('https://s3.amazonaws.com/codecademy-content/courses/ltp4/emails-api/emails.json')
+            .success(function (data) {
               return data;
             })
-            .error(function(err) {
+            .error(function (err) {
               return err;
             });
 }]);
