6c0bf18c66ef436461269f8ea0fa1bbb4b16141d
Jared Lundholm <lundholmj@gmail.com>
2016-07-15 07:49:49 -0600

    finished angular reader

---
+++ codecademy/angularjs/reader/js/app.js
@@ -0,0 +1,15 @@
+var app = angular.module('ReaderApp', ['ngRoute']);
+        app.config(['$routeProvider', function ($routeProvider) {
+            $routeProvider.when('/books', {
+                controller: "BookshelfController",
+                templateUrl: "views/bookshelf.html"
+            }).when('/books/:book', {
+                controller: 'BookController',
+                templateUrl: 'views/book.html'
+            }).when('/books/:book/chapters/:chapter', {
+                controller: 'ChapterController',
+                templateUrl: 'views/chapter.html'
+            }).otherwise({
+                redirectTo: '/books'
+            });
+        }]);
